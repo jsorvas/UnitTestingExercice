600ccedbd86c0d0379af51f731e93c4560545f48
Jordan <jordan.sorvas.pro@gmail.com>
2020-01-17 12:57:11 +0100

    Initial commit

---
+++ src/test/java/testRectangle.java
@@ -0,0 +1,10 @@
+package Exercices;
+
+public class testRectangle {
+    public static void main(String [] args){
+
+        Rectangle monRectangle = new Rectangle (4.0,5.0);
+        System.out.println("Test OK");
+        monRectangle.afficher();
+    }
+}
